cb7981b126b885cb2492d98dd2f4129d490a44a4
Steven Ortega <stevencortega99@gmail.com>
2024-03-04 02:26:25 -0600

    added some basic equations for finding the ht coeff

--- heat_transfer_scripts/convective_flow.py
+++ heat_transfer_scripts/convective_flow.py
@@ -1,29 +1,124 @@
 import numpy as np
+from pprint import pprint
 import load
+from load import unitMismatchError
 """
 This file computes heat transfer using forced convection.
 
 Authors:
     Steven ortega
 
-
 Assumptions made:
+    - Dittus-Boelter - Fully developed turbulent flow in a circular tube
+    - Using a smooth channel
+    - Circular channels
 
+Notes:
+    Before each calculation, specify the units needed for the equation.
+    If the units for the property set in the yaml file don't match, an
+    exception is raised to notify the user.
+        psuedocode:         if yaml_diameter['units'] != 'mm:
+                                raise unitMismatchError(...)
+    
+    If you convert units, then check that the unit comming in matches the
+    conversion factor you specify.
+        i.e.            YAML file says diameter in [mm]; Equation needs [m]
+        psuedocode:
+                        if yaml_diameter['units'] == 'mm':
+                            diameter = {'val': yaml_diameter/1000, 'unit': 'm'}
+                        else:
+                            raise unitMismatchError(...)
 
 """
 
-# this file works in metric values
-metric_units = [
-    'mm', 'm',      # Length
-    'm^2'           # Area
-    'm^3', 'L'      # Volume
-    'Kg',           # Mass
-    'N',            # Force
-    's',            # Time
-    'W',            # Power
-    'K', 'C',       # Temperature
-    'Pa',           # Pressure
-    'J'             # Energy
-]
+calcs = {}
 
+#---------------------
+# Setup the parameters
+#---------------------
 params = load.load_config('heat_transfer_scripts/setup.yaml')
+
+try:
+    load.checkUnits(params['cooling_jacket'])
+    load.checkUnits(params['fluid_properties'])
+except Exception as e:
+    print(f'##########################\n')
+    print(e)
+    print(f'\n##########################')
+
+jacket_p    = params['cooling_jacket']
+fluid_p     = params['fluid_properties']
+
+
+#----------------
+# Reynolds Number
+#----------------
+# equation: Re = rho V D/ mu
+
+rho = fluid_p['density']
+V = fluid_p['velocity']
+D = jacket_p['ID']
+mu = fluid_p['viscosity']
+
+if rho['unit'] != "Kg/m^3":
+    raise unitMismatchError(f"density, {rho['unit']}")
+if V['unit'] != "m/s":
+    raise unitMismatchError(f"velocity, {V['unit']}")
+if mu['unit'] != "Kg/m.s":
+    raise unitMismatchError(f"viscosity, {mu['unit']}")
+if D['unit'] == "mm":
+    D = {'val' : D['val'] / 1000, 'unit': 'm'}
+else:
+    raise unitMismatchError(f"diameter, {D['unit']}")
+
+calcs['Re'] = (rho['val'] * V['val'] * D['val'])/mu['val']
+
+#-----------------------
+# Get the Prandtl Number
+#-----------------------
+# eq: mu Cp / k
+
+cp = fluid_p['cp']
+k = fluid_p['k']
+mu = fluid_p['viscosity']
+
+if cp['unit'] != "J/Kg.K":
+    raise unitMismatchError(f"cp, {cp['unit']}")
+if k['unit'] != "W/m.K":
+    raise unitMismatchError(f"k, {k['unit']}")
+if mu['unit'] != "Kg/m.s":
+    raise unitMismatchError(f"viscosity, {mu['unit']}")
+
+calcs['Pr'] = (cp['val'] * mu['val'])/ k['val']
+
+#-----------------------
+# Get the Nusselt Number
+#-----------------------
+# Dittus–Boelter Eq
+# eq: 0.023 Re^.8 Pr^.4
+
+calcs['Nu'] = 0.023 * (calcs['Re']**.8) * (calcs['Pr']**.4)
+
+#----------------------------------
+# Get the Heat Transfer Coefficient
+#----------------------------------
+# For a smooth channel
+# eq: Nu K / D
+k = fluid_p['k']
+D =  jacket_p['ID']
+
+if k['unit'] != "W/m.K":
+    raise unitMismatchError(f"k, {k['unit']}")
+if D['unit'] == "mm":
+    D = {'val' : D['val'] / 1000, 'unit': 'm'}
+else:
+    raise unitMismatchError(f"diameter, {D['unit']}")
+
+calcs['h'] = (calcs['Nu'] * k['val']) / D['val']
+
+#-----------------------------
+# See everything we calculated 
+print()
+pprint(calcs)
+print()
+#-----------------------------
--- heat_transfer_scripts/load.py
+++ heat_transfer_scripts/load.py
@@ -1,5 +1,62 @@
 import yaml
-import pprint
+from pprint import pprint
+import re
+
+# List of units that are valid 
+metric_units = [
+    'mm', 'm', 'cm',    # Length
+    'L',                # Volume
+    'Kg', 'g',          # Mass
+    'N',                # Force
+    's',                # Time
+    'W',                # Power
+    'K', 'C',           # Temperature
+    'Pa',               # Pressure
+    'J',                # Energy
+    'placeholder',      # Skip errors during dev
+    'na'                # No units
+]
+
+class InvalidUnitsError(Exception):
+    """Exception thrown when invalid units are detected."""
+    def __init__(self, bad_units):
+        self.bad_units = bad_units
+        super().__init__(f"Invalid units detected:\n{bad_units}")
+
+class unitMismatchError(Exception):
+    """Let the user know that there is a unit mismatch."""
+    def __init__(self, units):
+        self.units = units
+        msg = f"\n---------------------------------------"  \
+              f"\nUnit mismatch. Is this correct?\n{units}\n" \
+              f"---------------------------------------\n"
+        super().__init__(msg)
+
+def checkUnits(param_list: dict):
+    """Checks the units of the provided param_list against allowed units 
+    in the metric_units array.
+
+    Args:
+        param_list (dict): A dictionary with all the parameter names, values,
+                           and units for the parameter.
+
+    Raises:
+        InvalidUnitsError: Raised if the units are not in metric_units
+    """
+    invalid_units = []
+    for k, v in param_list.items():
+        units = re.findall(r'[a-zA-Z]+', v['unit'])
+        if not all(unit in metric_units for unit in units):
+            invalid_units.append(k)
+            print(f'\n---------------------------------------')
+            print(f'Is {v['unit']} a metric unit?')
+            print(f'---------------------------------------\n')
+
+    if invalid_units:
+        incorrect_units = ""
+        for p in invalid_units:
+            incorrect_units += f"{p}, {param_list[p]['unit']}"
+        raise InvalidUnitsError(incorrect_units)
 
 def load_config(file_path: str = "config.yaml") -> dict:
     """Reads the provided yaml file in file_path and extracts the parameters.
@@ -14,12 +71,12 @@ def load_config(file_path: str = "config.yaml") -> dict:
     with open(file_path) as stream:
         try:
             return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+        except yaml.YAMLError as e:
+            print(e)
 
 if __name__ == "__main__":
     parameters = load_config("heat_transfer_scripts/setup.yaml")
-    pprint.pprint(parameters)
+    pprint(parameters)
 
 
     
